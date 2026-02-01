d276c9d43f42cb50a49245883f531aa70fa0958b
Manuel <github@schiegg.at>
2026-02-01 21:39:41 +0100

    Fix gunicorn path

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ set -e
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
-gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 3 --timeout 60
+gunicorn core.wsgi:application --bind 0.0.0.0:8000 --workers 3 --timeout 60
