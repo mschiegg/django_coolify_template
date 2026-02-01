35d87ea21efe2cc8ea449919e550120004fb57b7
Manuel <github@schiegg.at>
2026-02-01 21:29:47 +0100

    Docker Test

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+set -e
+
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+
+gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 3 --timeout 60
